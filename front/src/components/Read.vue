e449a0f60ab14e9d02dbf20aba0a7c1e843df28b
Eom <Eom@DESKTOP-6S30BVC>
2020-11-01 15:56:20 +0900

    init commit

---
+++ front/src/components/Read.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+	<table>
+		<tr>
+			<td>글쓴이</td>
+			<td>제목</td>
+			<td>내용</td>
+		</tr>
+		<tr v-for="value in data" >
+			<td>글쓴이</td>
+			<td>제목</td>
+			<td>내용</td>
+		</tr>
+	</table>
+  </div>
+</template>
+
+<script>
+export default {
+	import data from "@/data";
+	name: 'Read',
+	data() {
+
+	},
+	
+}
+</script>
+
+<style>
+
+</style>
